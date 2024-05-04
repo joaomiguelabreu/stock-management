9493502bd19ed6ee59bd229e4b6dda7057fe51bc
joao <joaomiguelabreu11@gmail.com>
2024-05-04 19:37:22 +0100

    DashBoard_Arranjado

--- frontend/src/Pages/DashBoard.jsx
+++ frontend/src/Pages/DashBoard.jsx
@@ -1,18 +1,14 @@
-import React from 'react'
-import WhiteContainer from '../components/global/WhiteContainer'
+import React from "react";
+import WhiteContainer from "../components/global/WhiteContainer";
 
-const DashBoard = ({css}) => {
+const DashBoard = ({ css }) => {
   return (
-    <div className='flex flex-col bg-purple-400 h-screen'>
-      <div className='flex justify-center'>
-        <WhiteContainer
-          info={
-            <div>"gsrgseges"</div>
-          }
-        />
+    <div className="flex flex-col bg-purple-400 h-screen">
+      <div className="flex justify-center">
+        <WhiteContainer info={<div>"gsrgseges"</div>} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashBoard
+export default DashBoard;
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DropDown from './global/Dropdown';
+import DropDown from "./global/Dropdown";
 
 // -----------ICONS-------------
 import { AiFillDashboard } from "react-icons/ai";
@@ -10,20 +10,24 @@ import { Link } from "react-router-dom";
 const SideBar = () => {
   return (
     <div className="w-72 bg-gray-800 text-gray-300 ">
-      <div className="flex flex-col ml-5 mr-5 justify-between " >
-      <DropDown
-        buttonContent={
-          <div className='flex flex-row items-center gap-4'>
-            <div>Nome</div>
-            <div className='bg-blue-400 rounded-full h-10 w-10'></div>
-          </div>}
-        dropdownContent={
-          <div className="absolute mt-2 right-0 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul className=" text-sm text-gray-700 dark:text-gray-200">
-              <div className='block px-4 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white hover:rounded-lg cursor-pointer transition duration-300 ease-in-out'>Logout</div>
-            </ul>
-          </div>}
-      />
+      <div className="flex flex-col ml-5 mr-5 justify-between ">
+        <DropDown
+          buttonContent={
+            <div className="flex flex-row items-center gap-4">
+              <div>Nome</div>
+              <div className="bg-blue-400 rounded-full h-10 w-10"></div>
+            </div>
+          }
+          dropdownContent={
+            <div className="absolute mt-2 right-0 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+              <ul className=" text-sm text-gray-700 dark:text-gray-200">
+                <div className="block px-4 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white hover:rounded-lg cursor-pointer transition duration-300 ease-in-out">
+                  Logout
+                </div>
+              </ul>
+            </div>
+          }
+        />
 
         <div className="flex flex-col gap-3 ">
           <div>
@@ -62,11 +66,7 @@ const SideBar = () => {
           </div>
         </div>
       </div>
-
-
-
     </div>
-
   );
 };
 
