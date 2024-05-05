80678f682c34d09e90fb5f6b86d8632a9814b806
MargaridaHenriques <margaridahenriques2020@gmail.com>
2024-05-05 19:10:38 +0100

    Revert "react_redux"
    This reverts commit 2c1d3edb0c3aa799d6cff85b9c25bb5a6409ab48.

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,97 +1,115 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { json } from "react-router-dom";
 import Loading from "../components/Loading";
 import ErrorMessage from "../components/ErrorMessage";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { login } from "../actions/userActions";
 
 const Login = ({ history }) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  const [error, seterror] = useState(false);
+  const [loading, setloading] = useState(false);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { loading, error, userInfo } = userLogin
+  const submitHandler = async (e) => {
+    e.preventDefault();
 
-    useEffect(() => {
-        if (userInfo) {
-            navigate("/DashBoard")
-        }
-    }, [history, userInfo])
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
 
-    const submitHandler = async (e) => {
-        e.preventDefault();
+      setloading(true);
 
-        dispatch(login(email, password, navigate));
-    };
+      const { data } = await axios.post(
+        "http://localhost:5555/users/login",
+        {
+          email,
+          password,
+        },
+        config
+      );
 
-    return (
-        <div>
-            <div>LOGIN</div>
-            {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-            {loading && <Loading />}
-            <form onSubmit={submitHandler} class="max-w-sm mx-auto">
-                <div class="mb-5">
-                    <label
-                        for="email"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                    >
-                        Your email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="name@flowbite.com"
-                        required
-                    />
-                </div>
-                <div class="mb-5">
-                    <label
-                        for="password"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                    >
-                        Your password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required
-                    />
-                </div>
-                <div class="flex items-start mb-5">
-                    <div class="flex items-center h-5">
-                        <input
-                            id="remember"
-                            type="checkbox"
-                            value=""
-                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                            required
-                        />
-                    </div>
-                    <label
-                        for="remember"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-black"
-                    >
-                        Remember me
-                    </label>
-                </div>
-                <button
-                    type="submit"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                    Submit
-                </button>
-            </form>
+      //ALTERAR
+      console.log(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      navigate("/DashBoard");
+      //------------------------------
+    } catch (error) {
+      seterror(error.response.data.message);
+      setloading(false);
+    }
+  };
+
+  return (
+    <div>
+      <div>LOGIN</div>
+      {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+      {loading && <Loading />}
+      <form onSubmit={submitHandler} class="max-w-sm mx-auto">
+        <div class="mb-5">
+          <label
+            for="email"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="name@flowbite.com"
+            required
+          />
         </div>
-    );
+        <div class="mb-5">
+          <label
+            for="password"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <div class="flex items-start mb-5">
+          <div class="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              required
+            />
+          </div>
+          <label
+            for="remember"
+            class="ms-2 text-sm font-medium text-gray-900 dark:text-black"
+          >
+            Remember me
+          </label>
+        </div>
+        <button
+          type="submit"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
