3c8a16c68912c674edfaceaa7af3f46baf91a4f6
MargaridaHenriques <margaridahenriques2020@gmail.com>
2024-05-05 14:22:48 +0100

    fixed_errror

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,100 +1,99 @@
 import React, { useState } from "react";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setpassword] = useState("");
-  const [error, seterror] = useState(false);
-  const [loading, setloading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setpassword] = useState("");
+    const [error, seterror] = useState(false);
+    const [loading, setloading] = useState(false);
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+    const submitHandler = async (e) => {
+        e.preventDefault();
 
-    try {
-      const config = {
-        headers: {
-          "Content-type":"application/json"
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json"
+                }
+            }
+
+            setloading(true);
+
+            const { data } = await axios.post('/users/login', {
+                email,
+                password,
+            }, config);
+
+            setloading(false);
+        } catch (error) {
+            setloading(false);
+            console.error('Error:', error.message);
         }
-      }
+    };
 
-      setloading(true)
 
-      const {data}=await axios.post('/users/login',
-        
-      ){
-        
-email,
-        password,
-        },config}
-
-      setloading(false)
-    } catch (error) {
-      
-    }
-  };
-
-  return (
-    <div>
-      <div>LOGIN</div>
-      <form onSubmit={submitHandler} class="max-w-sm mx-auto">
-        <div class="mb-5">
-          <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-          >
-            Your email
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
-            required
-          />
+    return (
+        <div>
+            <div>LOGIN</div>
+            <form onSubmit={submitHandler} class="max-w-sm mx-auto">
+                <div class="mb-5">
+                    <label
+                        for="email"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                        Your email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="name@flowbite.com"
+                        required
+                    />
+                </div>
+                <div class="mb-5">
+                    <label
+                        for="password"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                        Your password
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setpassword(e.target.value)}
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required
+                    />
+                </div>
+                <div class="flex items-start mb-5">
+                    <div class="flex items-center h-5">
+                        <input
+                            id="remember"
+                            type="checkbox"
+                            value=""
+                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                            required
+                        />
+                    </div>
+                    <label
+                        for="remember"
+                        class="ms-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                        Remember me
+                    </label>
+                </div>
+                <button
+                    type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                    Submit
+                </button>
+            </form>
         </div>
-        <div class="mb-5">
-          <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-          >
-            Your password
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setpassword(e.target.value)}
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          />
-        </div>
-        <div class="flex items-start mb-5">
-          <div class="flex items-center h-5">
-            <input
-              id="remember"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-              required
-            />
-          </div>
-          <label
-            for="remember"
-            class="ms-2 text-sm font-medium text-gray-900 dark:text-black"
-          >
-            Remember me
-          </label>
-        </div>
-        <button
-          type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default Login;
