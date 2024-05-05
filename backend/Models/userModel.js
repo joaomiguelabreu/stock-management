864c7e92b760a8e6e516630a63b65d1daf9ff423
MargaridaHenriques <margaridahenriques2020@gmail.com>
2024-05-05 12:28:19 +0100

    password_encrypt_fixed

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -28,12 +28,18 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    console.error("Error encrypting password:", error);
+    next(error);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
