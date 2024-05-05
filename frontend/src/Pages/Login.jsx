80006fdb9f5c1663a7b84e132c9d45f67b1c0402
MargaridaHenriques <margaridahenriques2020@gmail.com>
2024-05-05 14:48:42 +0100

    fixed_localhost

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
       setloading(true);
 
       const { data } = await axios.post(
-        "/users/login",
+        "http://localhost:5555/users/login",
         {
           email,
           password,
@@ -33,6 +33,7 @@ const Login = () => {
       console.log(data);
       localStorage.setItem("userInfo", JSON.stringify(data));
       //------------------------------
+
       setloading(false);
     } catch (error) {
       seterror(error.response.data.message);
@@ -42,11 +43,11 @@ const Login = () => {
   return (
     <div>
       <div>LOGIN</div>
-      <form onSubmit={submitHandler} class="max-w-sm mx-auto">
-        <div class="mb-5">
+      <form onSubmit={submitHandler} className="max-w-sm mx-auto">
+        <div className="mb-5">
           <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
           >
             Your email
           </label>
@@ -55,15 +56,15 @@ const Login = () => {
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="name@flowbite.com"
             required
           />
         </div>
-        <div class="mb-5">
+        <div className="mb-5">
           <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
           >
             Your password
           </label>
@@ -72,30 +73,30 @@ const Login = () => {
             id="password"
             value={password}
             onChange={(e) => setpassword(e.target.value)}
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
           />
         </div>
-        <div class="flex items-start mb-5">
-          <div class="flex items-center h-5">
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
             <input
               id="remember"
               type="checkbox"
               value=""
-              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
               required
             />
           </div>
           <label
-            for="remember"
-            class="ms-2 text-sm font-medium text-gray-900 dark:text-black"
+            htmlFor="remember"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-black"
           >
             Remember me
           </label>
         </div>
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
         </button>
