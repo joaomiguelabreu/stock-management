a4e440ed9b01c4b6cbde94ae3f98302f57ad0931
MargaridaHenriques <margaridahenriques2020@gmail.com>
2024-05-05 11:48:40 +0100

    creating_models

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
